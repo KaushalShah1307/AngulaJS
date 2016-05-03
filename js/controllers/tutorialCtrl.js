9f3ea1fd0977bcb3c37dd8746f2f5a4af8267efa
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-02 23:59:34 -0400

    usage of defining variables to the scope in the controller and referencing these variables in index.html

--- js/controllers/tutorialCtrl.js
+++ js/controllers/tutorialCtrl.js
@@ -4,5 +4,17 @@
 angular.module("tutorialCtrlModule", [])
 
 .controller("tutorialCtrl", ["$scope", function ($scope) {
+
+    $scope.message = "Hello World!";
+    $scope.tutorialCtrlObject = {};
+
+    $scope.tutorialCtrlObject.title = "My First Angular Page!";
+    $scope.tutorialCtrlObject.subTitle = "I`m very excited.";
+
+    $scope.tutorialCtrlObject.binding = 2;
+
+    $scope.multiply = function () {
+        $scope.tutorialCtrlObject.binding *= 2;
+    }
     
 }]);
