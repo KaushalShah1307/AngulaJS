07273a107af9863ed24093dabd324ad84ddce244
Kaushal Shah <KaushalShah1307@gmail.com>
2016-05-05 01:28:40 -0400

    changing the code to learn and apply github routing and moving concepts

--- js/app.js
+++ js/app.js
@@ -2,4 +2,21 @@
  * Created by kshah on 5/2/16.
  */
 
-var app = angular.module("tutorialApp", ["tutorialCtrlModule"]);
+var app = angular.module("tutorialApp", ["ngRoute","tutorialCtrlModule"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+
+
+        .when("/", {
+
+            templateUrl: "newPage.html",
+            controller: "tutorialCtrl"
+        })
+
+        .when("/newPage", {
+            redirectTo: "newPage2.html",
+            controller: "tutorialCtrl"
+        })
+
+}]);
